92229c7d5c7082cd24bc6dfc27b6848234f13a1b
Atheer Ganayem <atheer95@gmail.com>
2025-06-24 15:13:06 +0300

    backend url in next.config.ts

--- client-linux/next.config.ts
+++ client-linux/next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   env: {
-    NEXTAUTH_URL: "http://192.168.1.16:3000",
-    BACKEND_URL: "http://192.168.1.16:8080",
-    WS_URL: "ws://192.168.1.16:8080",
+    // NEXTAUTH_URL: "http://192.168.1.16:3000",
+    BACKEND_URL: "https://chatifiy-3.fly.dev",
+    WS_URL: "wss://chatifiy-3.fly.dev",
     AWS: "https://atheer-web-projects.s3.eu-central-1.amazonaws.com/",
   },
   /* config options here */
